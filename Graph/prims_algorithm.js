845c4d39a49fa1a8867638434c7dd5485209c64a
wyattsweet <wyattsweet@gmail.com>
2019-01-01 21:20:40 -0800

    added some comments

--- Graph/prims_algorithm.js
+++ Graph/prims_algorithm.js
@@ -10,44 +10,52 @@ const graph = [
 ]
 
 function prim(graph) {
+  // store all vertices that have a pair
   const visited = [];
+  // store the pairs
   const pairs = [];  
 
+  // start it off by pushing the first vertex onto the visited array
   visited.push(0);
 
   while(visited.length < graph.length) {
 
+    // visited vertex with lowest weight edge
     let parent;
+    // neightboring vertex
     let pair;
+    // the weight of the edge that connects them
     let pairWeight;
 
-    const visitedCopy = visited.slice();
-
-    visitedCopy.forEach(vertex => {
-      // get the edges of the vertex at i
-      const edges = graph[vertex];
-      // lowest weight, grab the first weight in the array greater than 0
+    // iterate over the visited vertices to find the one with the lowest weight
+    visited.forEach(parentVertex => {
+      // get the edges of the visited vertex
+      const edges = graph[parentVertex];
+      // variable to store the edge with the lowest weight
       let lowest;
       // vertex, or index of lowest weight edge
-      let pairIndex;
+      let lowestIndex;
 
+      // iterate over the edges
       for (let x = 0; x < edges.length; x++) {
         const value = edges[x];
 
-
+        // crazy logic if the edge is greater than 0 and not in the visited array and lowest is either undefined or the value is lower than what's currently there
         if (value > 0 && !visited.includes(x) && (!lowest || value < lowest)) {
           lowest = value;
-          pairIndex = x;
+          lowestIndex = x;
         }
       }
 
+      // after iterating over the edges store the lowest in pair if it's undefined or lower than what's currently there
       if (!pair || lowest < pairWeight) {
-        parent = vertex;
-        pair = pairIndex;
+        parent = parentVertex;
+        pair = lowestIndex;
         pairWeight = lowest;
       }
     })
 
+    // save the final lowest pair to the pairs array and update visited
     visited.push(pair);
     pairs.push({ edges: [parent, pair], weight: pairWeight })
   }
