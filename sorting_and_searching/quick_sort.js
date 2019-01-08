384a7381161fec3bb47a55dbab5e5788a270dac5
wyattsweet <wyattsweet@gmail.com>
2019-01-07 21:11:22 -0800

    add comments to quickSort

--- sorting_and_searching/quick_sort.js
+++ sorting_and_searching/quick_sort.js
@@ -2,10 +2,15 @@ function quickSort(origArr) {
   if (origArr.length <= 1) {
     return origArr;
   }
+  // copy the original array as to not mutate the input
   const arr = origArr.slice();
+  // grab the last element as the pivot
   const pivot = arr.pop();
+  // for values lower than the pivot
   const left = [];
+  // for values greater than the pivot
   const right = [];
+  // for the pivot and all values equal to it
   const center = [pivot];
 
   for (let i = 0; i < arr.length; i++) {
@@ -23,6 +28,7 @@ function quickSort(origArr) {
     }
   }
   
+  // call quickSort again with the greater and less than partitions
   const leftSorted = quickSort(left);
   const rightSorted = quickSort(right);
 
