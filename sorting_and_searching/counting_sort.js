478fbc6783880b641fc161ec1a8fcfd660cb9beb
wyattsweet <wyattsweet@gmail.com>
2019-01-09 22:24:12 -0800

    finished counting sort

--- sorting_and_searching/counting_sort.js
+++ sorting_and_searching/counting_sort.js
@@ -1,11 +1,25 @@
-function findArrIndex(max) {
-  const ceil = max / 10;
-  return Number.isInteger(ceil) ? max + 9 : Math.ceil(ceil) * 10 - 1;
-}
-
-function countingSort(arr) {
+function countingSort(origArr) {
+  const arr = origArr.slice();
   const max = Math.max(...arr);
-  console.log(findArrIndex(max));
+  const countArr = Array(max + 1).fill(0);
+  const orderedArr = Array(arr.length - 1);
+
+  arr.forEach(num => {
+    countArr[num] += 1;
+  });
+
+  for (let i = 0; i < countArr.length - 1; i++) {
+    countArr[i + 1] = countArr[i] + countArr[i +  1];
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+    const place = countArr[val];
+    orderedArr[place - 1] = val;
+    countArr[val] -= 1;
+  }
+
+  return orderedArr;
 }
 
-countingSort([100]);
+console.log(countingSort([344, 212, 566, 234]));
