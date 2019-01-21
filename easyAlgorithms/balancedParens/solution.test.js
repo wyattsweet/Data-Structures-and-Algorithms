d8116fd4ab8cd87626a9d6213419f62bfbd69dc9
wyattsweet <wyattsweet@gmail.com>
2019-01-20 23:29:42 -0800

    added tests for balanced parens

--- easyAlgorithms/balancedParens/solution.test.js
+++ easyAlgorithms/balancedParens/solution.test.js
@@ -2,6 +2,22 @@ import test from 'ava';
 
 import balancedParens from './solution'
 
-test('foo', t => {
-  t.pass();
+test('returns true when parens are balanced', t => {
+  const response = balancedParens('when(will(we leave (earth) on a ((space ship))))')
+  t.true(response);
 });
+
+test('returns false when parens are unbalanced', t => {
+  const response = balancedParens('when(will(we leave (earth) on a ((space ship))')
+  t.false(response);
+})
+
+test('returns true', t => {
+  const response = balancedParens('()');
+  t.true(response);
+})
+
+test('returns false when input is not a string', t => {
+  const response = balancedParens(true);
+  t.false(response);
+})
